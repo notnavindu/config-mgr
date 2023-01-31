231a10f15d4c5fd9d37ec6efb19777953e3ae812
Navindu <navanavi3@gmail.com>
2023-01-31 18:34:53 +0530

    fix: cors

--- src/routes/api/macrostash/+server.ts
+++ src/routes/api/macrostash/+server.ts
@@ -1,6 +1,19 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
  
+/** @type {import('./$types').RequestHandler} */
+export async function OPTIONS() {
+	return new Response(null, {
+		status: 200,
+		statusText: 'OK',
+		headers: {
+			'content-type': 'application/json',
+			'access-control-allow-origin': '*'
+		}
+	});
+}
+
+
 export const GET = (() => {
   return json({
 		name: "MacroStash",
